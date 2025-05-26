a54243a94c07466e4a9be4a8b424bb18427d9e45
mr-ssc <soyabchhuvara3@gmail.com>
2025-05-26 12:51:23 +0530

    first commit

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -5,9 +5,9 @@ import './Hero.css';
 const Hero = () => {
   return (
     <div className='Hero-hero-container'>
-      <img 
-        src={logo} 
-        alt="Full screen logo" 
+      <img
+        src={logo}
+        alt="Full screen logo"
         className="Hero-full-screen-image"
       />
     </div>
